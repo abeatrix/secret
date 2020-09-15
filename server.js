55f294cefc54934644f6094f0bbc37a9b192d6bd
Beatrix Woo <beatrixwho@gmail.com>
2020-09-15 16:52:05 -0700

    server setup with methodoverride and controllers

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
+//EXTERNAL MODULES
 const express = require('express');
-const app = express();
+const controllers = require('./controllers');
+const methodOverride = require('method-override');
+const path = require('path');
 
+//INTERNAL MODULES
 const PORT = 4000;
 
-const contollers = require('./controllers');
-const methodOverride = require('method-override');
+//INSTANCED MODULES
+const app = express();
+
+
+
+//MIDDLEWARE
+app.unsubscribe(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`)
+    next();
+})
+
+
+app.set('view engine', 'ejs')
+
+app.use('/whisperer', controllers.whisperer)
+
+
 
 // SERVER LISTENER
 app.listen(PORT, () => {
     console.log(`Listening for secrets on port ${PORT}`);
 });
 
-
-app.set('view engine', 'ejs')
 
 // 404
 app.use({}, ()=> {
