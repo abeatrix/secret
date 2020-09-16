58e42c8e76e619d3f2c1e8712d754a25cdb2c79a
Beatrix Woo <beatrixwho@gmail.com>
2020-09-15 20:32:56 -0700

    express sessions installed

--- models/Whisperer.js
+++ models/Whisperer.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
+
+
 //Schema ('template', option configurational obj)
 const WhispererSchema = new mongoose.Schema(
     {
         username: {type: String, required: [true, 'you must provide an email as your username.'], index: { unique: true }},
         name: {type: String, required: true},
         password: {type: String, required: true},
-        birthday: {type: String, required: true, default: Date.now},
-    }
+        createdAt: {type: Number, required: true, default: Date.now},
+    },
+    { timestamps: { createdAt: 'created_at' } }
 )
 
 const Whisperer = mongoose.model('Whisperer', WhispererSchema);
